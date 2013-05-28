679841ebf79c1106addde6453ae4e62ef8c76ea1
Thomas W Bell <thomaswbell88@gmail.com>
2013-05-28 01:01:13 +0100

    Initial commit; Added a visual frame to image blocks

---
+++ themeconfig.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @author Thomas W Bell http://thomaswbell.co.uk
+ */
+
+$theme = new StdClass;
+
+$theme->displayname  = 'Trophy';
+$theme->parent = 'default';
+$theme->formrenderer = 'table';
